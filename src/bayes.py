6d764f405130f701b55a6ea57f268f39057e15d2
Armstrong Hirwa <a.hirwa2@alustudent.com>
2026-07-07 12:15:19 +0200

    some returned text to what was deleted

--- src/bayes.py
+++ src/bayes.py
@@ -87,3 +87,89 @@ keyword_reviews = df[
 keyword_count = len(keyword_reviews)
 
 print(f"Count of reviews containing '{keyword}': {keyword_count}")
+
+#===============================
+# Count the marginal (P(keyword)) All reviews with keyword / Total reviews
+#===============================
+
+marginal = keyword_count / total_reviews
+
+print(f"Marginal probability of '{keyword}': {marginal}")
+
+
+#===============================
+# Count the Bayes theorem (P(positive | keyword)) Likelihood * Prior / Evidence
+#===============================
+
+posterior = (
+    likelihood
+    * prior
+) / marginal
+
+print(f"Posterior probability of positive sentiment given '{keyword}': {posterior:.2f}")
+
+
+#=========================================
+# Reusable function for Bayes theorem
+#=========================================
+print("\n================\n BAYES RESULTS\n================\n")
+print(
+    f"{'---------------------------------------------------------------------------------------\n'}"
+    f"| {'Keywords':<15} |"
+    f"{'Prior':>15} |"
+    f"{'Likelihood':>15} |"
+    f"{'Marginal':>15} |"
+    f"{'Posterior':>15} |"
+    f"{'\n---------------------------------------------------------------------------------------'}"
+)
+def bayes_keyword(df, keyword_input):
+
+    total_reviews = len(df)
+
+    positive_reviews = len(df[df["sentiment"] == "positive"])
+
+    prior = positive_reviews / total_reviews
+
+    positive_keyword = len(
+        df[
+            (df["sentiment"] == "positive")
+            &
+            (
+                df["review"]
+                .str.lower()
+                .str.contains(keyword_input)
+            )
+        ]
+    )
+
+    likelihood = positive_keyword / positive_reviews
+
+    keyword_count = len(
+        df[
+            df["review"]
+            .str.lower()
+            .str.contains(keyword_input)
+        ]
+    )
+
+    marginal = keyword_count / total_reviews
+
+    posterior = (likelihood * prior) / marginal
+
+    print(
+        f"| {keyword_input:<15} |"
+        f"{prior:>15.4f} |"
+        f"{likelihood:>15.4f} |"
+        f"{marginal:>15.4f} |"
+        f"{posterior:>15.4f} |"
+    )
+
+
+#===========================
+# Calling bayes function
+#===========================
+
+keywords = ["excellent", "great", "amazing", "love"]
+
+for keyword_input in keywords:
+    bayes_keyword(df, keyword_input)
